5b9382f42266a87178224fc617acfc2668718c56
Ernesto Guevara Ramírez <eguevarar@gmail.com>
2020-10-17 08:49:06 -0400

    Add Database and ORM related functionality

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ContactModule } from './contact/contact.module';
+import { configService } from './config/config.service';
 
 @Module({
-  imports: [ContactModule],
+  imports: [
+    ContactModule,
+    TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
