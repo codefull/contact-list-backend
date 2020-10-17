ad4191108ca5058fb7345160eb6acb38e05deda6
Ernesto Guevara Ramírez <eguevarar@gmail.com>
2020-10-17 16:50:25 -0400

    Controller refactoring. added validation and http codes to response

--- src/contact/contact.controller.ts
+++ src/contact/contact.controller.ts
@@ -6,7 +6,10 @@ import {
   Delete,
   Param,
   Body,
+  Res,
+  HttpStatus,
 } from '@nestjs/common';
+import { Response } from 'express';
 import { ContactService } from './contact.service';
 import { Contact } from './contact.model';
 
@@ -15,53 +18,105 @@ export class ContactController {
   constructor(private readonly contactsService: ContactService) {}
 
   @Get()
-  getContacts(): Contact[] {
+  async getContacts(): Promise<Contact[]> {
     return this.contactsService.getAllContacts();
   }
 
   @Get(':id')
-  getContact(@Param('id') contactId: string): Contact {
+  async getContact(@Param('id') contactId: string): Promise<Contact> {
     const contact = this.contactsService.getSingleContact(contactId);
-    return { ...contact };
+    return contact;
   }
 
   @Post()
-  addContact(
+  async addContact(
     @Body('name') cname: string,
     @Body('address') caddress: string,
     @Body('email') cemail: string,
     @Body('phone') cphone: string,
-  ): any {
-    const generatedId = this.contactsService.insertContact(
-      cname,
-      caddress,
-      cemail,
-      cphone,
-    );
-    return { id: generatedId };
+    @Body('avatar') cavatar: string,
+    @Res() res: Response,
+  ): Promise<Response<{ id: string }>> {
+    if ( this.emptyFields('valid', cname, caddress, cemail, cphone, cavatar, false))
+      return res.status(HttpStatus.BAD_REQUEST).json({
+        id: 'All fields are required (name, address, email, phone, avatar)',
+      });
+
+    try {
+      const idObj = await this.contactsService.insertContact(
+        cname,
+        caddress,
+        cemail,
+        cphone,
+        cavatar,
+      );
+      return res.status(HttpStatus.OK).json({ ...idObj });
+    } catch (error) {
+      return res
+        .status(HttpStatus.BAD_REQUEST)
+        .json({ id: `${error.message}` });
+    }
   }
 
   @Patch(':id')
-  updateContact(
+  async updateContact(
     @Param('id') contactId: string,
     @Body('name') cname: string,
     @Body('address') caddress: string,
     @Body('email') cemail: string,
     @Body('phone') cphone: string,
-  ): Contact {
-    const c = this.contactsService.updateContact(
+    @Body('avatar') cavatar: string,
+    @Res() res: Response
+  ): Promise<Response<Contact>> {
+    if (
+      this.emptyFields(
+        contactId,
+        cname,
+        caddress,
+        cemail,
+        cphone,
+        cavatar,
+        true,
+      )
+    )
+    return res.status(HttpStatus.BAD_REQUEST).json({
+      id: 'There must be at least one valid field',
+    });  
+    
+    const c = await this.contactsService.updateContact(
       contactId,
       cname,
       caddress,
       cemail,
       cphone,
+      cavatar,
     );
-    return { ...c };
+    return res.status(HttpStatus.OK).json({ ...c });
   }
 
   @Delete(':id')
-  deleteContact(@Param('id') contactId: string): Contact {
-    const contact = this.contactsService.deleteContact(contactId);
-    return { ...contact };
+  async deleteContact(@Param('id') contactId: string, @Res() res: Response
+  ): Promise<Response<Contact>> {
+    if (!contactId) {
+      return res.status(HttpStatus.BAD_REQUEST).json({
+        id: 'Invalid field (id)',
+      }); 
+    }
+    const contact = await this.contactsService.deleteContact(contactId);
+    return res.status(HttpStatus.OK).json({ ...contact });
+  }
+
+  emptyFields(
+    contactId: string,
+    cname: string,
+    caddress: string,
+    cemail: string,
+    cphone: string,
+    cavatar: string,
+    all = true,
+  ) {
+    return all
+      ? !contactId && !cname && !caddress && !cemail && !cphone && !cavatar
+      : !contactId || !cname || !caddress || !cemail || !cphone || !cavatar;
   }
 }
