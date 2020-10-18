63c251d0e7f625c44de8645d652055a489ef440f
Ernesto Guevara Ramírez <eguevarar@gmail.com>
2020-10-18 14:57:06 -0400

    Refactoring data response to manage errors

--- src/contact/contact.controller.ts
+++ src/contact/contact.controller.ts
@@ -23,9 +23,35 @@ export class ContactController {
   }
 
   @Get(':id')
-  async getContact(@Param('id') contactId: string): Promise<Contact> {
-    const contact = this.contactsService.getSingleContact(contactId);
-    return contact;
+  async getContact(
+    @Param('id') contactId: string,
+    @Res() res: Response,
+  ): Promise<Response<{ data: Contact; error: any }>> {
+    try {
+      const contact = await this.contactsService.getSingleContact(contactId);
+      let response;
+      if (!contact) {
+        response = res.status(HttpStatus.NOT_FOUND).json({
+          data: {},
+          error: {
+            message: 'Contact Not Found',
+          },
+        });
+      } else {
+        response = res.status(HttpStatus.OK).json({
+          data: { ...contact },
+        });
+      }
+
+      return response;
+    } catch (error) {
+      return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+        data: [],
+        error: {
+          message: error.message,
+        },
+      });
+    }
   }
 
   @Post()
@@ -36,10 +62,16 @@ export class ContactController {
     @Body('phone') cphone: string,
     @Body('avatar') cavatar: string,
     @Res() res: Response,
-  ): Promise<Response<{ id: string }>> {
-    if ( this.emptyFields('valid', cname, caddress, cemail, cphone, cavatar, false))
+  ): Promise<Response<{ data: { id: string }; error: any }>> {
+    if (
+      this.emptyFields('valid', cname, caddress, cemail, cphone, cavatar, false)
+    )
       return res.status(HttpStatus.BAD_REQUEST).json({
-        id: 'All fields are required (name, address, email, phone, avatar)',
+        data: { id: '' },
+        error: {
+          message:
+            'All fields are required (name, address, email, phone, avatar)',
+        },
       });
 
     try {
@@ -50,11 +82,12 @@ export class ContactController {
         cphone,
         cavatar,
       );
-      return res.status(HttpStatus.OK).json({ ...idObj });
+      return res.status(HttpStatus.OK).json({ data: { ...idObj } });
     } catch (error) {
-      return res
-        .status(HttpStatus.BAD_REQUEST)
-        .json({ id: `${error.message}` });
+      return res.status(HttpStatus.BAD_REQUEST).json({
+        data: { id: '' },
+        error: { message: `${error.message}` },
+      });
     }
   }
 
@@ -66,8 +99,8 @@ export class ContactController {
     @Body('email') cemail: string,
     @Body('phone') cphone: string,
     @Body('avatar') cavatar: string,
-    @Res() res: Response
-  ): Promise<Response<Contact>> {
+    @Res() res: Response,
+  ): Promise<Response<{ data: Contact; error: any }>> {
     if (
       this.emptyFields(
         contactId,
@@ -79,10 +112,11 @@ export class ContactController {
         true,
       )
     )
-    return res.status(HttpStatus.BAD_REQUEST).json({
-      id: 'There must be at least one valid field',
-    });  
-    
+      return res.status(HttpStatus.BAD_REQUEST).json({
+        data: { id: '' },
+        error: { message: 'There must be at least one valid field' },
+      });
+
     const c = await this.contactsService.updateContact(
       contactId,
       cname,
@@ -91,19 +125,22 @@ export class ContactController {
       cphone,
       cavatar,
     );
-    return res.status(HttpStatus.OK).json({ ...c });
+    return res.status(HttpStatus.OK).json({ data: { ...c } });
   }
 
   @Delete(':id')
-  async deleteContact(@Param('id') contactId: string, @Res() res: Response
-  ): Promise<Response<Contact>> {
+  async deleteContact(
+    @Param('id') contactId: string,
+    @Res() res: Response,
+  ): Promise<Response<{ data: Contact; error: any }>> {
     if (!contactId) {
       return res.status(HttpStatus.BAD_REQUEST).json({
-        id: 'Invalid field (id)',
-      }); 
+        data: { id: '' },
+        error: { message: 'Invalid field (id)' },
+      });
     }
     const contact = await this.contactsService.deleteContact(contactId);
-    return res.status(HttpStatus.OK).json({ ...contact });
+    return res.status(HttpStatus.OK).json({ data: { ...contact } });
   }
 
   emptyFields(
